20b1a15d0e0a0003d4b4ee1baf3b1faf5eb37256
computerphilosopher <abc3242@gmail.com>
2019-04-04 01:28:20 +0900

    change file field to image field

--- upload/models.py
+++ upload/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 class UploadFileModel(models.Model):
     title = models.TextField(default='', null = True)
-    file = models.FileField(null=False)
+    file = models.ImageField(null=False)
+
+    def __str__(self):
+        return self.title
