b29ec9bdefad8aa1bc8e5d9864414cb9495482a4
computerphilosopher <abc3242@gmail.com>
2019-04-04 03:01:42 +0900

    moviepy가 동작함을 확인

--- upload/models.py
+++ upload/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class UploadFileModel(models.Model):
     title = models.TextField(default='', null = True)
-    file = models.ImageField(null=False)
+    file = models.ImageField(upload_to='upload/origin', null=False)
+    converted = models.FileField(upload_to='upload/converted', null = False,default='')
 
     def __str__(self):
         return self.title
--- upload/views.py
+++ upload/views.py
@@ -2,21 +2,37 @@ from rest_framework import viewsets
 #from upload.serializers import UnitSerializer
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.conf import settings
 from upload.models import UploadFileModel
 from upload.forms import UploadFileForm
+import moviepy.editor as mp
 
 # Create your views here.
     
 
 def download_video(request):
+    
     return render(request, 'download_page.html')
+
+def convert(instance):
+
+    root = settings.MEDIA_ROOT 
+    
+    file_url = root + instance.file.url.split('/')[-1]
+    clip = mp.VideoFileClip(file_url)
+
+    video_name = (file_url.split('/')[-1]).split('.')[0]
+    clip.write_videofile(video_name)
+
+    instance.converted = video_name
 
 
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            instance = form.save()
+            convert(instance)
             return redirect('download/')
     else:
         form = UploadFileForm()
