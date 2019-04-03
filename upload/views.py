c2c04b3409ae2936c0309683c13c96ad2ed5dbbb
computerphilosopher <abc3242@gmail.com>
2019-04-04 01:09:26 +0900

    브랜치 임무 완료

--- upload/views.py
+++ upload/views.py
@@ -7,11 +7,15 @@ from upload.forms import UploadFileForm
 
 # Create your views here.
     
+def handle_file(file):
+    return 0
+
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            handle_file(request.FIELS)
     else:
         form = UploadFileForm()
     return render(request, 'upload_page.html', {'form': form})
