0f07b349aa213ce3b06354e47b7c714e1639cfde
computerphilosopher <erlk0nig@naver.com>
2019-04-04 09:01:49 +0000

    add converted directory

--- MillenniumFilecon/settings.py
+++ MillenniumFilecon/settings.py
@@ -155,7 +155,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'www_dir ', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
