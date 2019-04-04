babe8b5bbce7372ec4adf9936f521f4828e2ef1e
computerphilosopher <erlk0nig@naver.com>
2019-04-04 17:24:50 +0900

    settings.py 수정

--- MillenniumFilecon/settings.py
+++ MillenniumFilecon/settings.py
@@ -36,9 +36,9 @@ def get_secret(setting, secrets=secrets):
 
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
-ALLOWED_HOSTS = ['*']
+DEBUG = True
+#DEBUG = False
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
