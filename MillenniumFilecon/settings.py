3ec10ec829ed4decf6e4a85ac17c90d7574422be
computerphilosopher <erlk0nig@naver.com>
2019-04-04 08:38:05 +0000

    successed migration in python anywhere

--- MillenniumFilecon/settings.py
+++ MillenniumFilecon/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['computerphilosopher.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -155,7 +155,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'www_dir ', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
